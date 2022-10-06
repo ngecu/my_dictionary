e6c034a5faec68e491222fc333c8cd527ba65b64
DevNgecu <devngecu@gmail.com>
2022-10-06 23:24:14 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import {Badge} from 'react-bootstrap';
+
+
+import './bootstrap.min.css';
+import './style.css';
+
+import {Alert} from 'react-bootstrap';
 
 function App() {
+    const [historywords,setHistorywords] = useState([])
+    const [searchword,setSearchword] = useState("")
+    const[foundresult,setFoundResult] = useState(true)
+    const [meaning,setMeaning] = useState("")
+    const [phonetic,setPhonetic] = useState("")
+
+
+    
+
+  const  fetchWord = (word) =>{
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(response => {
+      const data = response.data[0]
+
+      console.log(data)
+      // changing phonetic
+      const {phonetic} = data
+      setPhonetic(phonetic)
+
+
+      const meanings = data.meanings
+      const {definitions} =meanings[0]
+      setMeaning(definitions[0].definition)
+
+      console.log(foundresult)
+
+      setFoundResult(true)
+     
+      if (!historywords.includes(searchword)) {
+        setHistorywords(current => [...current, searchword]);
+      }
+
+      if (historywords.length > 10) {
+        historywords.shift()
+      }
+
+      
+
+
+     
+    })
+    .catch((error)=>{
+      console.log(`error is ${error} `)
+      setFoundResult(false)
+    })
+    
+  }
+  console.log("how is histor wotd",historywords);
+
+  // console.log(searchwordresult);
+
+
+
+  useEffect(() => {
+    fetchWord(searchword)
+  }, [searchword])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+<div className="wrapper">
+      <header>My Dictionary &#128218;</header>
+      <div className="search">
+        <input 
+        type="text" 
+        placeholder="Search a word" 
+        required 
+        spellCheck="false"
+        // value={searchword}
+        onKeyUp={(e)=>setSearchword(e.target.value)}
+        />
+        <i className="fas fa-search"></i>
+        <span className="material-icons">close</span>
+      </div>
+      <p className="info-text">
+        {searchword.length > 0 ? (
+          <>
+   {
+      foundresult ? (
+        <Alert key='success' variant='success'>
+          Result found
+        </Alert>
+   ):(
+    <Alert key='danger' variant='danger'>
+       No Result found
+  </Alert>    
+      )}
+          </>
+        ):(
+          <Alert key='danger' variant='danger'>
+          Type in for search &#9757;&#65039;
+        </Alert>
+
+        )}
+     
+      </p>
+      <ul>
+        <li className="word">
+          <div className="details">
+            <p>{searchword}</p>
+            <span>{phonetic}</span>
+          </div>
+       
+        </li>
+        {
+          foundresult ? (
+
+            <div className="content">
+            <li className="meaning">
+              <div className="details">
+                <p>Meaning</p>
+                <span>{meaning}</span>
+              </div>
+            </li>
+           
+          </div>
+          ): (
+            <> 
+            
+            </>
+          )
+        }
+
+      </ul>
     </div>
+
+    <div className="footer">
+  {historywords.length > 0 && historywords.map((word)=>(
+   
+    <Badge
+    onClick={()=>setSearchword(word)}
+    className="mx-2" bg="dark">{word}</Badge>
+  ))}
+</div>
+    </>
   );
 }
 
